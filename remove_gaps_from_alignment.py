349b52dd39832c8eecbee762bbd448f6f3b04588
Liubov Chuprikova <shlykovalv@gmail.com>
2017-07-29 18:48:33 +0200

    some minor corrections

--- remove_gaps_from_alignment.py
+++ remove_gaps_from_alignment.py
@@ -9,19 +9,20 @@ from Bio.Alphabet import Gapped
 from Bio import motifs
 
 ALPHABET = Gapped(IUPAC.ambiguous_dna, gap_char='-')
+UNAMBIGUOUS_ALPHABET = Gapped(IUPAC.unambiguous_dna, gap_char='-')
 
 
 def cleanup_alignment(alignment):
     alignment_modified = MultipleSeqAlignment([])
 
-    for record in alignment:
-        if record.id == '#=GC_RF':
+    for rec in alignment:
+        if rec.id == '#=GC_RF':
             break
-        alignment_modified.append(record)
+        alignment_modified.append(rec)
 
-    for record in alignment_modified:
-        record.seq = record.seq.upper()
-        record.seq = Seq(str(record.seq).replace('.', '-'), ALPHABET)
+    for rec in alignment_modified:
+        rec.seq = rec.seq.upper()
+        rec.seq = Seq(str(rec.seq).replace('.', '-'), ALPHABET)
 
     return alignment_modified
 
@@ -40,18 +41,18 @@ def determine_gap_positions(motif, is_unchangeable):
 
 
 def remove_gaps_from_alignment(alignment, species):
-    alignment_for_species = MultipleSeqAlignment([r for r in alignment if r.id in species], ALPHABET)
+    alignment_for_species = MultipleSeqAlignment([rec for rec in alignment if rec.id in species], ALPHABET)
 
-    whole_motif = motifs.create([r.seq for r in alignment], ALPHABET)
-    species_motif = motifs.create([r.seq for r in alignment_for_species], ALPHABET)
+    whole_motif = motifs.create([rec.seq for rec in alignment], ALPHABET)
+    species_motif = motifs.create([rec.seq for rec in alignment_for_species], ALPHABET)
 
     gap_positions_species = determine_gap_positions(species_motif, True)
     gap_positions_whole = determine_gap_positions(whole_motif, False)
     positions_to_remove = set(gap_positions_species).intersection(gap_positions_whole)
 
-    for r in alignment:
-        seq_string_wo_gaps = ''.join([r.seq[i] for i in range(len(r.seq)) if i not in positions_to_remove])
-        r.seq = Seq(seq_string_wo_gaps, ALPHABET)
+    for rec in alignment:
+        string_wo_gaps = ''.join([rec.seq[i] for i in range(len(rec.seq)) if i not in positions_to_remove])
+        rec.seq = Seq(string_wo_gaps, ALPHABET)
 
     return alignment
 
@@ -60,8 +61,9 @@ CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
 
 @click.command(context_settings=CONTEXT_SETTINGS)
-@click.option('--rdb-alignment-file', '-a', type=click.File('r'))
-@click.option('--species-file', '-sp', type=click.File('r'))
+@click.option('--rdb-alignment-file', '-a', type=click.File('r'), help='Alignment in FASTA format')
+@click.option('--species-file', '-sp', type=click.File('r'),
+              help='File with taxon representatives each on its own line')
 @click.option('--output-file', '-o', type=click.File('w'))
 def foo(rdb_alignment_file, species_file, output_file):
     alignment = AlignIO.read(rdb_alignment_file, format='fasta')
