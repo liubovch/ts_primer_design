e066b720272f0cd2ebee9ef89be8af2e44a4ec8f
Liubov Chuprikova <shlykovalv@gmail.com>
2017-08-11 19:15:31 +0200

    edited CLI options

--- sort_alignment_acto_tree.py
+++ sort_alignment_acto_tree.py
@@ -1,22 +1,24 @@
 #!/usr/bin/env python3
 
 import click
+import os.path as path
 from Bio import Phylo
 from Bio import AlignIO
 
+
+__TREE_PATH = path.join(path.dirname(__file__), 'HITdb_v1.00', 'HITdb.tre')
 
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
 
 @click.command(context_settings=CONTEXT_SETTINGS)
-@click.option('--alignment', '-a', type=click.File('r'), help='Alignment in FASTA format')
+@click.option('--alignment', '-a', type=click.File('r'),
+              help='Alignment in FASTA format (after removing gaps)', required=True)
 @click.option('--species', '-sp', type=click.File('r'),
-              help='File with taxon representatives each on its own line')
-@click.option('--tree', '-tr', type=click.File('r'),
-              help='Phylogenetic tree for species in alignment in NEWICK format')
-@click.option('--output', '-o', type=click.File('w'))
-def sort_alignment_acto_tree(alignment, species, tree, output):
-    tree = Phylo.read(tree, format='newick')
+              help='File with taxon representatives each on its own line', required=True)
+@click.option('--output', '-o', type=click.File('w'), required=True)
+def sort_alignment_acto_tree(alignment, species, output):
+    tree = Phylo.read(__TREE_PATH, format='newick')
     leaves = tree.get_terminals()
 
     species = [line.strip() for line in species]
