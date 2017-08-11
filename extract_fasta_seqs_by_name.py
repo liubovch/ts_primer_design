2e744c779982d278fbab2f7d0693b8403e8428bf
Liubov Chuprikova <shlykovalv@gmail.com>
2017-08-11 19:34:29 +0200

    edited CLI options

--- extract_fasta_seqs_by_name.py
+++ extract_fasta_seqs_by_name.py
@@ -1,19 +1,22 @@
 #!/usr/bin/env python3
 
 import click
+import os.path as path
 from Bio import SeqIO
+
+
+__SEQUENCES_PATH = path.join(path.dirname(__file__), 'HITdb_v1.00', 'HITdb_sequences.fasta')
 
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
 
 @click.command(context_settings=CONTEXT_SETTINGS)
-@click.option('--species', '-sp', type=click.File('r'), help='File with taxon representatives each on its own line')
-@click.option('--sequences', '-sq', type=click.File('r'),
-              help='File in FASTA format from which you want to extract sequences')
-@click.option('--output', '-o', type=click.File('w'))
-def extract_fasta_seqs_by_name(species, sequences, output):
+@click.option('--species', '-sp', type=click.File('r'),
+              help='File with taxon representatives each on its own line', required=True)
+@click.option('--output', '-o', type=click.File('w'), required=True)
+def extract_fasta_seqs_by_name(species, output):
     species = [line.strip() for line in species]
-    for rec in SeqIO.parse(sequences, 'fasta'):
+    for rec in SeqIO.parse(__SEQUENCES_PATH, 'fasta'):
         if rec.id in species:
             print('>' + rec.id, file=output)
             print(str(rec.seq), file=output)
